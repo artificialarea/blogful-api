a040c76ecb7dca477c034b9bf122dfa58ad897f1
Sacha Sedriks <sachased@gmail.com>
2020-07-19 11:34:00 -0700

    moved test FIXTURE from spec file to it's own file and function

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai')
 const knex = require('knex')
 const app = require('../src/app');
+const { makeArticlesArray } = require('./articles.fixtures')
 const supertest = require('supertest');
 
 describe.only('Articles Endpoints', () => {
@@ -23,36 +24,7 @@ describe.only('Articles Endpoints', () => {
     context('Given there are articles in the database', () => {
 
         // FIXTURE
-        let testArticles = [ 
-            {
-                id: 1,
-                date_published: '2029-01-22T16:28:32.615Z',
-                title: 'First test post',
-                style: 'How-to',
-                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'
-            },
-            {   
-                id: 2,
-                date_published: '2100-05-22T16:28:32.615Z',
-                title: 'Second test post',
-                style: 'News',
-                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
-            },
-            {   
-                id: 3,
-                date_published: '1919-12-22T16:28:32.615Z',
-                title: 'Third test post',
-                style: 'Listicle',
-                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
-            },
-            {   
-                id: 4,
-                date_published: '1919-12-22T16:28:32.615Z',
-                title: 'Fourth test post',
-                style: 'Story',
-                content: 'Lorem ipsum dolor sit amet. Necessitatibus.'
-            },
-        ];
+        const testArticles = makeArticlesArray()
 
         beforeEach('insert articles', () => {
             return db
