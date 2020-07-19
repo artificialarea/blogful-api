16b6e9f77815781d21b7914098f5dff49922be16
Sacha Sedriks <sachased@gmail.com>
2020-07-19 12:49:08 -0700

    refactored tests so there is a describe (with multiple contexts) for each endpoint

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -4,7 +4,7 @@ const app = require('../src/app');
 const { makeArticlesArray } = require('./articles.fixtures')
 const supertest = require('supertest');
 
-describe.only('Articles Endpoints', () => {
+describe('Articles Endpoints', () => {
 
     let db;
 
@@ -21,30 +21,62 @@ describe.only('Articles Endpoints', () => {
     before('clean the table', () => db('blogful_articles').truncate());
     afterEach('cleanup', () => db('blogful_articles').truncate());
 
-    context('Given there are articles in the database', () => {
+    describe('GET /articles', () => {
 
-        // FIXTURE
-        const testArticles = makeArticlesArray()
-
-        beforeEach('insert articles', () => {
-            return db
-                .into('blogful_articles')
-                .insert(testArticles)
+        context('Given there are articles in the database', () => {
+            const testArticles = makeArticlesArray()
+    
+            beforeEach('insert articles', () => {
+                return db
+                    .into('blogful_articles')
+                    .insert(testArticles)
+            });
+    
+            it('responds with 200 and all of the articles', () => {
+                return supertest(app)
+                    .get('/articles')
+                    .expect(200, testArticles)
+            });
         });
 
-        it('GET /articles responds with 200 and all of the articles', () => {
-            return supertest(app)
-                .get('/articles')
-                .expect(200, testArticles)
+        context('Given the database is empty', () => {
+            it('responds with 200 and an empty list', () => {
+                return supertest(app)
+                    .get('/articles')
+                    .expect(200, [])
+            });
         });
 
-        it('GET /articles/:article_id responds with 200 and the specified article', () => {
-            const articleId = 2;
-            const expectedArticle = testArticles[articleId - 1]
-            return supertest(app)
-                .get(`/articles/${articleId}`)
-                .expect(200, expectedArticle)
-        })
+    });
+
+    describe('GET /articles/:article_id', () => {
+
+        context('Given there are articles in the database', () => {
+            const testArticles = makeArticlesArray()
+    
+            beforeEach('insert articles', () => {
+                return db
+                    .into('blogful_articles')
+                    .insert(testArticles)
+            });
+    
+            it('responds with 200 and the specified article', () => {
+                const articleId = 2;
+                const expectedArticle = testArticles[articleId - 1]
+                return supertest(app)
+                    .get(`/articles/${articleId}`)
+                    .expect(200, expectedArticle)
+            })
+        });
+
+        context(`Given the database is empty`, () => {
+            it(`responds with 404`, () => {
+                return supertest(app)
+                    .get(`/articles/123456`)
+                    .expect(404, { error: { message: `Article doesn't exist` } })
+            });
+        });
+
     });
 
 });
