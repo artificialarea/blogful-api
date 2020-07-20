4f2ea92436402c66686a754fb31c712b8a9af5b3
Sacha Sedriks <sachased@gmail.com>
2020-07-20 13:35:50 -0700

    added commentary re: xss

--- src/app.js
+++ src/app.js
@@ -24,13 +24,16 @@ app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
-// For XSS Sanitation
-// to simulate a Cross-site scripting (XSS) hack! 
-// src: https://courses.thinkful.com/node-postgres-v1/checkpoint/16#cross-site-scripting-xss-
+// For XSS Sanitation Demo Purposes ////////////////////////////////
 app.get('/xss', (req, res) => {
     res.cookie('secretToken', '1234567890');
     res.sendFile(__dirname + '/xss-example.html')
 });
+// ^^ to simulate a Cross-site scripting (XSS) hack! 
+// src: https://courses.thinkful.com/node-postgres-v1/checkpoint/16#cross-site-scripting-xss-
+// Typically one would remove this /xss route 
+// and the xss-example.html once finished, 
+// but I'll keep it for learning purposes.
 
 app.use(errorHandler);
 
