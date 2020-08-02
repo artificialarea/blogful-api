c9f100a2f52da7886c54a9da593cb40dc649d31f
Sacha Sedriks <sachased@gmail.com>
2020-08-02 12:31:33 -0700

    CRUCIAL UPDATE! for heroku-postgresql migration to work. See postgrator-config.js for troubleshooting scenario.

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,37 @@ require('dotenv').config();
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
+    "ssl": !!process.env.SSL,   // ThinkChat addition
     "connectionString": (process.env.NODE_ENV == 'test')
         ? process.env.TEST_DATABASE_URL
-        : process.env.DATABASE_URL,
+        : process.env.DATABASE_URL
 }
+
+
+/* RE: CRUCIAL UPDATE! commit 
+
+Followed instructions for Deploying database 
+per: https://courses.thinkful.com/node-postgres-v1/checkpoint/20 
+but reached an impasse when trying to run migrations on heroku-postresql
+
+$ npm run migrate:production
+kept fialing at the blogful-api migrate script
+postgrator --config postgrator-config.js "not found"
+
+Thanks to a long troubleshooting session with Jonathan Huxhold @ ThinkfulChat
+issue eventually solved.
+
+Several changes were made to .env, package.json, config.js, and postgrator-config.js
+so I'm not sure which particular change (or combination of changes) was 
+the deciding factor.
+
+The last change made for migration to work was done in this postgrator-config,
+by adding "ssl": !!process.env.SSL
+
+Jonathan says, 
+"I think postgrator does something under the hood to accommodate ssl settings 
+on db up in the environment. 
+The double bang (!!) casts a truthy or falsy value to a boolean true or false. 
+So by adding it as an actual postgrator config value, it was able to establish the connection."
+
+*/
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://ckvhtlhgiatvzz:760179d1df8258601211f8509c349fec075cd49572a6d0333ffddded84ec4a20@ec2-18-211-48-247.compute-1.amazonaws.com:5432/d9gqom6bk5vm9m',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful-test',
 }
